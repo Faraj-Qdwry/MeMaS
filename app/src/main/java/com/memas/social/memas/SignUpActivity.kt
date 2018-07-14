6dc8dff73bb3f574f88a7ad446be3474301ad031
FarajAg <farajalqdwry@gmail.com>
2018-07-14 18:27:47 +0800

    second

---
+++ app/src/main/java/com/memas/social/memas/SignUpActivity.kt
@@ -0,0 +1,19 @@
+package com.memas.social.memas
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.signup.*
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        signupButton.setOnClickListener { startActivity(Intent(this@SignUpActivity,MainActivity::class.java)) }
+
+        loginTextView.setOnClickListener { startActivity(Intent(this@SignUpActivity,LoginActivity::class.java)) }
+
+    }
+}
